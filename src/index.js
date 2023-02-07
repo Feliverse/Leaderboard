b8634e4a44a04974137e2ef453ebf4c6cd844ae0
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-02-07 17:44:26 -0400

    Html markup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-function component() {
-    const element = document.createElement('div');
-  
-     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+import './style.css';
