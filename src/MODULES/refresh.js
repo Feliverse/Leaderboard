08a7d8eb29ae3278703cef512a459c61de630ed4
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-02-09 15:26:35 -0400

    apply eslint fixes

--- src/MODULES/refresh.js
+++ src/MODULES/refresh.js
@@ -1,21 +1,19 @@
-import { getScores } from "./consumeAPI.js";
+import { getScores } from './consumeAPI';
 
-const refreshButton = document.querySelector('.refresh-button')
+const refreshButton = document.querySelector('.refresh-button');
 
 const refreshScores = async () => {
   const { data, status } = await getScores();
 
   const scores = document.querySelector('.scores');
   if (status !== 200) {
-    scores.innerHTML = 'Error ${status}: ${data.message}';
+    scores.innerHTML = '';
   } else {
-    data.result.forEach(element => {
-      
+    data.result.forEach((element) => {
       const score = document.createElement('li');
       score.classList.add('score');
       score.innerHTML = `${element.user}:${element.score}`;
       scores.appendChild(score);
-
     });
   }
   return scores;
